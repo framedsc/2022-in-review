d4239d584ee833aed3b6ce11cec42194cef4f13e
originalnicodr <niconavall@gmail.com>
2023-12-14 19:27:19 -0300

    Deleted fullscreen utility method since its not being used in this project.

--- pages/utils/utils.ts
+++ pages/utils/utils.ts
@@ -177,21 +177,6 @@ const baseSearchData = {
     return images;
   }
   
-  // check if fullscreen support is available
-  export const getBrowserFullscreenElementProp = () => {
-    if (typeof document.fullscreenElement !== 'undefined') {
-      return 'fullscreenElement';
-    } else if (typeof document.mozFullScreenElement !== 'undefined') {
-      return 'mozFullScreenElement';
-    } else if (typeof document.msFullscreenElement !== 'undefined') {
-      return 'msFullscreenElement';
-    } else if (typeof document.webkitFullscreenElement !== 'undefined') {
-      return 'webkitFullscreenElement';
-    } else {
-      throw new Error('fullscreenElement is not supported by this browser');
-    }
-  }
-  
   export const extractTopLevelDomain = (url: string[]) => {
     if (!url.indexOf('//')) {
       return url;
