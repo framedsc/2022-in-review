dadf6282a73abef1410d2ad73e50ef1f72477739
originalnicodr <niconavall@gmail.com>
2023-12-16 17:19:25 -0300

    Improved homepage on cellphone.

--- components/home/year-cover.tsx
+++ components/home/year-cover.tsx
@@ -39,6 +39,7 @@ const coverContainerStyle: CSS.Properties = {
   position: 'relative',
   //width: '100%',
   //max-width: '600px', /* Set your desired max-width */
+  margin: '15px 0px',
   width: '480px',//'600px',
   height: '640px',//'800px',
   overflow: 'hidden',
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import Head from "next/head";
 import React from "react";
+import CSS from 'csstype';
 
 import { YearCover} from "@components/home";
+
+const coverFrameStyle: CSS.Properties = {
+  marginTop: '80px',
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  alignItems: 'center',
+  justifyContent: 'space-around'
+}
 
 const Home = () => {
   return (
@@ -9,7 +19,7 @@ const Home = () => {
       <Head>
         <title>A year of FRAMED</title>
       </Head>
-      <div className='Covers wrapper' style={{  marginTop: '80px', display: 'flex',  flexDirection: 'row', alignItems: 'center',  justifyContent: 'space-around'}}>
+      <div className='Covers wrapper' style={ coverFrameStyle }>
         { YearCover(2023) }
         { YearCover(2022) }
         { YearCover(2021) }
