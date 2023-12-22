0f8eec87e8c731ad33af05c1cfb964bd3f2e7070
originalnicodr <niconavall@gmail.com>
2023-12-22 01:14:56 -0300

    Changed the link in the navbar

--- components/global/navigation.tsx
+++ components/global/navigation.tsx
@@ -48,7 +48,7 @@ export const Navigation = () => {
             </Link>
           </li>
           <Link
-            href="https://framedsc.com/framed-wrapped/"
+            href="../"
             style ={ aYearofFramedStyle }
           >
             <img src="recap-nsub-logo.svg" style={ recapLogoStyle }/>
