3b156239cce40c3d3958f04b254e6377de204e2a
originalnicodr <niconavall@gmail.com>
2023-12-23 14:14:15 +0000

    Deploying to gh-pages from @ framedsc/framed-wrapped@ffacc34928582d0d1931dae521cf781361a98c45 🚀

---
+++ _next/static/chunks/pages/_year-data-1bf9d9e4683b6f7c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[144],{5249:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_year-data",function(){return u(5260)}])}},function(n){n.O(0,[298,260,774,888,179],function(){return n(n.s=5249)}),_N_E=n.O()}]);
