055fdeb248270fc444d32f632c6241fe785f713e
originalnicodr <niconavall@gmail.com>
2023-12-23 12:00:59 -0300

    Attempt to fix the fonts not being found on production.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,22 @@
 /**
 * @type {import('next').NextConfig}
 */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+//const path = import("path");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   images: {
     loader: 'akamai',
     path: '',
   },
   assetPrefix: './',
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
 };
 
 export default nextConfig;
