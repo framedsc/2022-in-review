0b2e4a48822a15783efb5c77d16d8f651f0e7640
originalnicodr <niconavall@gmail.com>
2024-01-01 12:12:46 -0300

    Updated the flavor text with the latest numbers, including the last two weeks of the year.

--- pages/2023.tsx
+++ pages/2023.tsx
@@ -12,22 +12,22 @@ const flavourText = {
   captivated the Framed community throughout the year.
   `],
   top10sys: [`Coming in strong and surprising no one, Cyberpunk 2077 continues to dominate
-  the Share Your Shot channel with a whopping 1624 shots shared in 2023,
+  the Share Your Shot channel with a whopping 1704 shots shared in 2023,
   no doubt spurred on by extensive game updates and DLC content released this year.
-  Hogwarts Legacy casts its way into the second highest position with 987 shots,
+  Hogwarts Legacy casts its way into the second highest position with 1008 shots,
   thanks to its vibrant imagining of the Hogwarts region and its impressively lush landscapes.
   Baldur's Gate 3, the year's biggest RPG and The Game Awards 2023's Game of the Year,
-  rolled third place in the rankings, with 575 shots. We can rest assured that the Internet
+  rolled third place in the rankings, with 613 shots. We can rest assured that the Internet
   will never be short on gorgeous portraits of Astarion, Shadowheart, and Lae’zel anytime soon.
   `],
-  top10hof: [`Across the 15000+ shots shared in 2023, a total of 1155 from
+  top10hof: [`Across the 15000+ shots shared in 2023, a total of 1162 from
   177 games were voted up to the Hall of Framed. At the top, perhaps unsurprisingly,
-  is Cyberpunk 2077 once again with 144 entries. Those dystopian vistas and neon-soaked portraits
+  is Cyberpunk 2077 once again with 145 entries. Those dystopian vistas and neon-soaked portraits
   never fail to wow us, placing the game back up there. Mirroring the same pattern of
   Share Your Shot trends, Hogwarts Legacy comes in at second place with 100 entries of mystical
   forests and spellbinding surroundings. In a close third, and most surprisingly,
   is psychological thriller Alan Wake 2. Released with only two months left in the year,
-  it surged quickly in popularity, landing it in our top 3 at 81 entries into the Hall of Framed.
+  it surged quickly in popularity, landing it in our top 3 at 82 entries into the Hall of Framed.
   `],
   busysys: [`This day saw a total of 92 shots posted, the record high of the year leading into a
   pretty busy February. Headlined by the recently-released Dead Space Remake with 13 shots,
